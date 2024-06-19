ce0f5b5e9efdfce517810068998e94600796020e
izzulmaula <izzulmaula91@gmail.com>
2024-06-19 22:08:15 +0700

    update frontend

---
+++ frontend/README.md
@@ -0,0 +1,26 @@
+# Reliji Trash Care
+
+## prerequisite
+
+Sebelum memulai, pastikan Anda telah memenuhi persyaratan berikut:
+- Anda telah menginstal [Git](https://git-scm.com/)
+- Anda telah menginstal [Node.js dan npm](https://nodejs.org/)
+
+## Instalasi
+
+### 1. Clone Repositori
+
+Pertama, clone repositori ini menggunakan Git:
+
+```bash
+git clone https://github.com/Reliji-Trash-Care/Front-End.git
+```
+
+### 2. Instal and Run
+
+Instal dependensi yang diperlukan di terminal menggunakan npm:
+
+```bash
+npm install
+npm run dev
+```
--- frontend/src/screens/InputBerita/InputBerita.jsx
+++ frontend/src/screens/InputBerita/InputBerita.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavbarUserFitur } from "../../components/NavbarUserFitur";
-import { SideBarWrapper } from "../../components/SideBarWrapper";
+import SideBarAdmin from "../../components/SideBarAdmin/SideBarAdmin";
 
 export const InputBerita = () => {
   return (
@@ -17,9 +17,9 @@ export const InputBerita = () => {
               </div>
               <div className="absolute w-[845px] h-[38px] top-[57px] left-[211px]">
                 <div className="absolute w-[142px] top-0 left-0 font-heading-8-bold font-[number:var(--heading-8-bold-font-weight)] text-black text-[length:var(--heading-8-bold-font-size)] tracking-[var(--heading-8-bold-letter-spacing)] leading-[var(--heading-8-bold-line-height)] [font-style:var(--heading-8-bold-font-style)]">
-                  Judul Berita
+                  Judul Berita 
                 </div>
-                <div className="absolute w-[715px] h-[28px] top-px left-[130px] bg-primary-1 rounded-[10px] border border-solid border-[#d9d9d9]" />
+                <input type="text" className="absolute w-[715px] h-[28px] top-px left-[130px] bg-primary-1 rounded-[10px] border border-solid border-[#d9d9d9] outline-none" />
               </div>
               <div className="absolute w-[81px] top-[228px] left-[211px] font-heading-8-bold font-[number:var(--heading-8-bold-font-weight)] text-black text-[length:var(--heading-8-bold-font-size)] tracking-[var(--heading-8-bold-letter-spacing)] leading-[var(--heading-8-bold-line-height)] [font-style:var(--heading-8-bold-font-style)]">
                 Isi Berita
@@ -28,33 +28,22 @@ export const InputBerita = () => {
                 Gambar
               </div>
               <div className="absolute w-[715px] h-[38px] top-[646px] left-[341px] bg-[#ffffffcc] rounded-[10px] border border-solid border-[#d9d9d9]">
-                <div className="absolute w-[90px] h-[22px] top-[7px] left-[12px] bg-[#d9d9d9b2] border border-solid border-[#818181]">
-                  <div className="absolute w-[80px] top-[2px] left-[4px] font-caption-1-regular font-[number:var(--caption-1-regular-font-weight)] text-black text-[length:var(--caption-1-regular-font-size)] text-center tracking-[var(--caption-1-regular-letter-spacing)] leading-[var(--caption-1-regular-line-height)] whitespace-nowrap [font-style:var(--caption-1-regular-font-style)]">
-                    Choose File
-                  </div>
-                </div>
-                <div className="absolute w-[145px] top-[9px] left-[109px] font-caption-1-regular font-[number:var(--caption-1-regular-font-weight)] text-[#505050] text-[length:var(--caption-1-regular-font-size)] tracking-[var(--caption-1-regular-letter-spacing)] leading-[var(--caption-1-regular-line-height)] whitespace-nowrap [font-style:var(--caption-1-regular-font-style)]">
-                  No file Choosen
-                </div>
+                <input id="myfile" type="file" className="absolute w-[200px] top-[9px]"/>
               </div>
-              <div className="absolute w-[715px] h-[377px] top-[229px] left-[341px] bg-primary-1 rounded-[10px] border border-solid border-variable-collection-7b7b7b" />
+              <input type="text" className="absolute w-[715px] h-[377px] top-[229px] left-[341px] bg-primary-1 rounded-[10px] border border-solid border-variable-collection-7b7b7b" />
               <div className="absolute w-[126px] h-[51px] top-[713px] left-[955px]">
                 <div className="relative w-[124px] h-[51px]">
-                  <div className="absolute w-[98px] h-[51px] top-0 left-0 bg-primary-2 rounded-[10px]" />
-                  <div className="absolute w-[107px] top-[14px] left-[17px] font-body-1-bold font-[number:var(--body-1-bold-font-weight)] text-white text-[length:var(--body-1-bold-font-size)] tracking-[var(--body-1-bold-letter-spacing)] leading-[var(--body-1-bold-line-height)] [font-style:var(--body-1-bold-font-style)]">
-                    {" "}
-                    Selesai
-                  </div>
+                  <input type="submit" className="absolute w-[98px] h-[45px] top-0 left-0 bg-primary-2 rounded-[10px] text-green-50" value="Submit" />
                 </div>
               </div>
               <div className="absolute w-[845px] h-[38px] top-[114px] left-[211px]">
                 <div className="absolute w-[142px] top-0 left-0 font-heading-8-bold font-[number:var(--heading-8-bold-font-weight)] text-black text-[length:var(--heading-8-bold-font-size)] tracking-[var(--heading-8-bold-letter-spacing)] leading-[var(--heading-8-bold-line-height)] [font-style:var(--heading-8-bold-font-style)]">
                   Nama Penulis
                 </div>
-                <div className="absolute w-[715px] h-[28px] top-px left-[130px] bg-primary-1 rounded-[10px] border border-solid border-[#d9d9d9]" />
+                <input type="text" className="absolute w-[715px] h-[28px] top-px left-[130px] bg-primary-1 rounded-[10px] border border-solid border-[#d9d9d9] outline-none" />
               </div>
               <div className="absolute w-[845px] h-[38px] top-[171px] left-[211px]">
-                <div className="absolute w-[715px] h-[28px] top-px left-[130px] bg-primary-1 rounded-[10px] border border-solid border-[#d9d9d9]" />
+                <input type="text" className="absolute w-[715px] h-[28px] top-px left-[130px] bg-primary-1 rounded-[10px] border border-solid border-[#d9d9d9] outline-none" />
                 <div className="absolute w-[142px] top-0 left-0 font-heading-8-bold font-[number:var(--heading-8-bold-font-weight)] text-black text-[length:var(--heading-8-bold-font-size)] tracking-[var(--heading-8-bold-letter-spacing)] leading-[var(--heading-8-bold-line-height)] [font-style:var(--heading-8-bold-font-style)]">
                   Nama Penebit
                 </div>
@@ -67,7 +56,7 @@ export const InputBerita = () => {
           profilOpenartImage="../../../static/img/openart-image-nfswrdqc-1711466557108-raw-2-3.png"
           profilText="Admin"
         />
-        <SideBarWrapper
+        <SideBarAdmin
           beritaPropertyDefaultClassName="!flex-[0_0_auto] !bg-secondary-1"
           chatText="Chat"
           className="!fixed !left-0 !top-0"
