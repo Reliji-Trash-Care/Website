db2d9b4714cbe58d109b3a3d9a77c343fae83a26
sopiaranty <sopiaranty@gmail.com>
2024-06-23 01:59:10 +0700

    perbarui tampilan jadwal

--- frontend/src/screens/JadwalSampahUser/JadwalSampahUser.jsx
+++ frontend/src/screens/JadwalSampahUser/JadwalSampahUser.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Calendar } from 'rsuite';
 import { NavbarUserFitur } from "../../components/NavbarUserFitur";
 import { SideBarWrapper } from "../../components/SideBarWrapper";
-import { Icon1 } from "../../icons/Icon1";
-import { TypeFiChevronLeftSize24ColorWhite } from "../../icons/TypeFiChevronLeftSize24ColorWhite";
 import { Button } from 'rsuite';
 import 'rsuite/styles/index.less'; // or 'rsuite/dist/rsuite.min.css'
 
@@ -16,22 +14,13 @@ export default App;
 export const JadwalSampahUser = () => {
   return (
     <div className="bg-white flex flex-row justify-center w-full">
-      <div className="bg-primary-1 overflow-hidden w-[1440px] h-[772px] relative">
+      <div className="bg-primary-1 overflow-hidden w-[1440px] h-[900px] relative">
         <div className="absolute w-[1674px] h-[1787px] top-[86px] left-[242px]">
           <div className="absolute w-[1674px] h-[1674px] top-[113px] left-0 bg-[#397d54b2] rounded-[837px] blur-[550px] opacity-[0.36]" />
-          <div className="flex flex-col w-[690px] h-[600px] items-start gap-[16px] p-[16px] absolute top-0 left-[388px] bg-primary-1 rounded-[10px] shadow-drop-shadow">
+          <div className="flex flex-col w-[690px] h-[780px] items-start gap-[16px] p-[16px] absolute top-0 left-[388px] bg-primary-1 rounded-[10px] shadow-drop-shadow-2">
             <div className="flex items-center justify-center gap-[32px] relative self-stretch w-full flex-[0_0_auto]">
-              <div className="flex w-[24px] h-[24px] items-center justify-center gap-[10px] p-[8px] relative rounded-[4px] border border-solid border-neutral-200">
-                <TypeFiChevronLeftSize24ColorWhite
-                  className="!relative !w-[16px] !h-[16px] !mt-[-4.00px] !mb-[-4.00px] !ml-[-4.00px] !mr-[-4.00px]"
-                  color="#64748B"
-                />
-              </div>
               <div className="relative flex-1 font-body-1-semibold font-[number:var(--body-1-semibold-font-weight)] text-neutral-800 text-[length:var(--body-1-semibold-font-size)] text-center tracking-[var(--body-1-semibold-letter-spacing)] leading-[var(--body-1-semibold-line-height)] [font-style:var(--body-1-semibold-font-style)]">
-                April 2024
-              </div>
-              <div className="flex w-[24px] h-[24px] items-center justify-center gap-[10px] p-[8px] relative rounded-[4px] border border-solid border-neutral-200 -rotate-180">
-                <Icon1 className="!relative !w-[16px] !h-[16px] !mt-[-4.00px] !mb-[-4.00px] !ml-[-4.00px] !mr-[-4.00px] !rotate-180" />
+                Kalender
               </div>
             </div>
             <div className="flex flex-col items-start relative flex-1 self-stretch w-full grow">
@@ -47,7 +36,7 @@ export const JadwalSampahUser = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col w-[282px] h-[97px] items-start justify-center gap-[10px] px-[16px] py-[8px] absolute top-[503px] left-[82px] bg-primary-1 rounded-[10px] shadow-drop-shadow">
+          <div className="flex flex-col w-[282px] h-[97px] items-start justify-center gap-[10px] px-[16px] py-[8px] absolute top-[503px] left-[82px] bg-primary-1 rounded-[10px] shadow-drop-shadow-2">
             <div className="flex flex-wrap w-[196px] items-start gap-[7px_18px] relative flex-[0_0_auto]">
               <div className="relative w-[24px] h-[24px] bg-primary-2 rounded-[12px]" />
               <div className="relative w-fit mt-[-1.00px] font-body-1-regular font-[number:var(--body-1-regular-font-weight)] text-tersier-2 text-[length:var(--body-1-regular-font-size)] text-center tracking-[var(--body-1-regular-letter-spacing)] leading-[var(--body-1-regular-line-height)] whitespace-nowrap [font-style:var(--body-1-regular-font-style)]">
